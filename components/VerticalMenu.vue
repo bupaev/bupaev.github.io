5efede6953adc345243fb4c8c5564b85066e770d
Pavel Buramensky <bupaev@gmail.com>
2021-08-07 19:17:27 +0600

    Improve code structure and naming in VerticalMenu component

--- components/VerticalMenu.vue
+++ components/VerticalMenu.vue
@@ -2,13 +2,16 @@
   <nav
     class="vertical-menu"
   >
-    <div ref="currentPosition" class="current-position" />
+    <div
+      :style="`transform: translateY(${markerOffset}px); height: ${markerHeight}px`"
+      class="visible-area-marker"
+    />
     <scrollactive ref="menu">
       <a
         v-for="item in items"
         :key="item.id"
-        class="item scrollactive-item"
         :href="`#${item.id}`"
+        class="item scrollactive-item"
       >
         <div class="item-icon">
           <v-icon color="#ff321c">
@@ -26,7 +29,7 @@
 <script>
 export default {
   name: 'VerticalMenu',
-  data () {
+  data() {
     return {
       items: [
         { title: 'Hello!', id: 'hero-area', icon: 'mdi-human-greeting' },
@@ -35,76 +38,62 @@ export default {
         { title: 'Experience', id: 'experience', icon: 'mdi-history' },
         { title: 'Contacts', id: 'contacts', icon: 'mdi-card-account-mail-outline' }
       ],
-      menuEl: null,
+      menuItemHeight: 0,
       contentSectionsHeightArray: [],
       contentSectionsOffsetArray: [],
-      menuItemHeight: 0,
-      coefficients: 0
+      scaleCoefficients: [],
+      markerOffset: 0,
+      markerHeight: 0
     }
   },
 
-  mounted () {
-    this.menuEl = this.$refs.menu
-    this.menuItemHeight = this.$refs.menu.$el.clientHeight / this.items.length
-    this.contentSectionsHeightArray = this.getSectionHeight()
-    this.contentSectionsOffsetArray = this.getSectionOffset()
+  mounted() {
+    this.menuItemHeight = this.$refs.menu.$el.getElementsByClassName('item')[0].clientHeight
+    this.contentSectionsHeightArray = this.getSectionsProp('clientHeight')
+    this.contentSectionsOffsetArray = this.getSectionsProp('offsetTop')
+
     // menu pixel per content pixel
-    this.coefficients = this.contentSectionsHeightArray.map((SectionsHeight) => {
+    this.scaleCoefficients = this.contentSectionsHeightArray.map((SectionsHeight) => {
       return this.menuItemHeight / SectionsHeight
     })
-    window.addEventListener('scroll', this.listenPageScroll)
+
+    window.addEventListener('scroll', this.setAreaMarkerPosition)
   },
 
-  destroyed () {
-    window.removeEventListener('scroll', this.listenPageScroll)
+  destroyed() {
+    window.removeEventListener('scroll', this.setAreaMarkerPosition)
   },
 
   methods: {
-    listenPageScroll () {
-      /* const contentHeight = document.body.scrollHeight
-      const currentScroll = window.scrollY
-      const percent = currentScroll / contentHeight
-      console.log('DEBUG: percent:', contentHeight, currentScroll, percent)
-      this.$refs.currentPosition.style.top = `${percent * 100}%` */
-      this.setCurrentPositionOffset()
-    },
-    getSectionHeight () {
+    getSectionsProp(propName) {
       const sectionsHTMLCollection = document.getElementsByClassName('page-section')
-      return [...sectionsHTMLCollection].map(({ clientHeight }) => clientHeight)
+      return [...sectionsHTMLCollection].map(section => section[propName])
     },
-    getSectionOffset () {
-      const sectionsHTMLCollection = document.getElementsByClassName('page-section')
-      return [...sectionsHTMLCollection].map(({ offsetTop }) => offsetTop)
-    },
-    // Menu items have equal heights but content sections have different heights.
-    // We have to map content sections height to menu items height
-    // to position the current position pointer correctly
-    setCurrentPositionOffset () {
-      // const contentHeight = document.body.scrollHeight
-      const currentStartScroll = window.scrollY
-      const currentEndScroll = currentStartScroll + window.innerHeight
-      // const percent = currentScroll / contentHeight
-
-      const currentStartSectionIndex = this.contentSectionsOffsetArray.findIndex((offset) => {
-        return currentStartScroll < offset
-      })
-      const currentEndSectionIndex = this.contentSectionsOffsetArray.findIndex((offset) => {
-        return currentEndScroll < offset
+    /**
+     * Menu items have equal heights but content sections have different heights.
+     * We have to map content sections offsets to menu items offsets
+     * to mark the current visible page area correctly
+     * @param {number} windowScroll
+     * @return {number}
+     */
+    getRescaledOffset(windowScroll) {
+      const sectionIndex = this.contentSectionsOffsetArray.findIndex((offset) => {
+        return windowScroll < offset
       })
 
-      const selectedStartMenuItem = Math.max(currentStartSectionIndex - 1, 0)
-      const selectedEndMenuItem = currentEndSectionIndex === -1
+      const selectedMenuItem = sectionIndex === -1
         ? this.contentSectionsOffsetArray.length
-        : Math.max(currentEndSectionIndex - 1, 0)
+        : Math.max(sectionIndex - 1, 0)
 
-      const startVisibleScreen = selectedStartMenuItem * this.menuItemHeight +
-        (currentStartScroll - this.contentSectionsOffsetArray[selectedStartMenuItem]) * this.coefficients[selectedStartMenuItem]
-      const endVisibleScreen = selectedEndMenuItem * this.menuItemHeight +
-        (currentEndScroll - this.contentSectionsOffsetArray[selectedEndMenuItem]) * this.coefficients[selectedEndMenuItem]
-      // console.log('DEBUG: selectedMenuItem, curPos:', selectedMenuItem, Math.round(curPos))
+      return selectedMenuItem * this.menuItemHeight +
+          (windowScroll - this.contentSectionsOffsetArray[selectedMenuItem]) * this.scaleCoefficients[selectedMenuItem]
+    },
+    setAreaMarkerPosition() {
+      const windowTopScrollY = window.scrollY
+      const windowBottomScrollY = windowTopScrollY + window.innerHeight
 
-      this.$refs.currentPosition.style.transform = `translateY(${startVisibleScreen}px)`
-      this.$refs.currentPosition.style.height = `${endVisibleScreen - startVisibleScreen}px`
+      this.markerOffset = this.getRescaledOffset(windowTopScrollY)
+      this.markerHeight = this.getRescaledOffset(windowBottomScrollY) - this.markerOffset
     }
   }
 }
@@ -119,7 +108,6 @@ export default {
   .item {
     display: flex;
     height: 50px;
-    // padding-top: 15px;
     cursor: pointer;
     color: #ff321c;
     text-decoration: none;
@@ -136,7 +124,7 @@ export default {
     }
   }
 
-  .current-position {
+  .visible-area-marker {
     position: absolute;
     left: -4px;
     height: 2em;
