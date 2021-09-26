6c6d1086d3775e5a202f809c0af10637abfe2bba
Pavel Buramensky <bupaev@gmail.com>
2021-09-27 02:39:27 +0600

    Improve text formatting in Hero Area

--- components/HeroArea.vue
+++ components/HeroArea.vue
@@ -3,48 +3,50 @@
     <div class="container">
       <div class="hero-body pa-0">
         <div class="columns is-mobile">
-          <div class="column is-two-thirds-mobile" style="z-index: 1;">
+          <div class="column is-two-thirds-mobile has-text-right" style="z-index: 1;">
             <h1 class="title pt-6">
               <div class="text-shape-limiter" />
               <span class="is-size-1">Hi! I’m Pavel Buramensky</span>
               <p class="is-size-2 pt-4">
-                I’m front-end developer who loves good UX-design and create things that make the world better
+                I’m front-end developer who loves good UX and create things which make the world better
               </p>
             </h1>
             <p class="subtitle is-size-4 pt-5">
               <a class="mr-5" href="mailto:me@paulbu.com">
                 <span class="icon-text">
-                  <span class="icon pr-1">
+                  <span class="icon">
                     <img src="../assets/icons/mail.svg">
                   </span>
-                  Email
+                  <span>Email</span>
                 </span>
               </a>
               <a class="mr-5" href="https://t.me/bupaev">
                 <span class="icon-text">
-                  <span class="icon pr-1">
+                  <span class="icon">
                     <img src="../assets/icons/telegram.svg">
                   </span>
-                  Telegram
-                </span></a>
+                  <span>Telegram</span>
+                </span>
+              </a>
               <a href="https://www.linkedin.com/in/pavel-buramensky/">
                 <span class="icon-text">
-                  <span class="icon pr-1">
+                  <span class="icon">
                     <img src="../assets/icons/linkedin.svg">
                   </span>
-                  LinkedIn
+                  <span>LinkedIn</span>
                 </span>
               </a>
             </p>
             <p class="subtitle is-size-4">
-              or just <a href="https://paulbu.com/pavel-buramensky-cv">
+              <span class="vertical-align-hack">also you can</span>
+              <a href="https://paulbu.com/pavel-buramensky-cv">
                 <span class="icon-text">
-                  <span class="icon pr-1">
+                  <span class="icon">
                     <img src="../assets/icons/download.svg">
                   </span>
-                  Download my CV
+                  <span>Download my CV</span>
                 </span>
-            </a>
+              </a>
             </p>
           </div>
           <div class="column">
@@ -73,13 +75,7 @@ export default {
 
   a {
     position: relative;
-    color: #363636;
-
-    /* transition: background 250ms ease;
-    background-image: linear-gradient(105deg, transparent 0, transparent 10px, gold 10px, gold 93%, transparent 93%, transparent 100%);
-    background-repeat: no-repeat;
-    background-size: 100%;
-    background-position-y: 1.2em; */
+    color: inherit;
 
     &::after {
       content: "";
@@ -95,10 +91,10 @@ export default {
     }
 
     &:hover {
+      color: #000;
+
       &::after {
-        height: 100%;
-        // transform: scale(1);
-        // background-size: 100% 100%;
+        height: 110%;
       }
     }
     &:active {
@@ -115,19 +111,17 @@ export default {
     font-weight: 500;
   }
 
+  .vertical-align-hack {
+    display: inline-block;
+    margin-top: -0.1em;
+    vertical-align: text-top;
+  }
+
   .text-shape-limiter {
     float: right;
     shape-outside: polygon(100% 0%, 100% 100%, 0 100%);
     width: 120px;
     height: 400px;
-  }
-
-  /* background-image:
-    repeating-linear-gradient(90deg, transparent 0, transparent 3px, white 3px, white 12px),
-    repeating-linear-gradient(180deg, rgba(0,0,0,0.1) 0, rgba(0,0,0,0.1) 3px, transparent 3px, transparent 12px);
-*/
-  .icon {
-    // filter: drop-shadow(1px 1px 0 rgba(130, 183, 255, 0.9));
   }
 
   .parallelogram-image-container {
