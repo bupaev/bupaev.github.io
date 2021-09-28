3bd056d0008b0b8bd8e9118514706a663f1c0151
Pavel Buramensky <bupaev@gmail.com>
2021-09-28 23:57:01 +0600

    Extract Synopsis to separate component

--- pages/index.vue
+++ pages/index.vue
@@ -11,37 +11,7 @@
       id="synopsis"
       class="section anchor-for-navigation"
     >
-      <div class="container">
-        <h2 class="title is-2">
-          Synopsis
-        </h2>
-        <div class="columns">
-          <div class="column is-one-third-desktop">
-            <VennDiagramProfessions />
-          </div>
-          <div class="column">
-            <div class="content">
-              <p>
-                Lead Front End Web Developer at CODENETIX,
-                Master Degree in Computer Science (July, 2011).
-                7+ years of software development experience.
-                10+ years of front end focused experience.
-                5 years of lecturing experience in Computer Science at Omsk State Technical University.
-              </p>
-              <p>
-                I love to create clean and smart cutting-edge user interfaces using modern web technologies with powerful and complex client-side logic behind UI. In addition, I would
-                like to work with Data Visualization and Data Mining.
-                I love to create clean and smart cutting-edge user interfaces using modern web technologies with powerful and complex client-side logic behind UI. In addition, I would
-                like to work with Data Visualization and Data Mining.
-              </p>
-              <p>
-                I love to create clean and smart cutting-edge user interfaces using modern web technologies with powerful and complex client-side logic behind UI. In addition, I would
-                like to work with Data Visualization and Data Mining.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Synopsis />
     </section>
     <section
       id="skills"
@@ -356,12 +326,12 @@
 </template>
 
 <script>
-import VennDiagramProfessions from '@/components/VennDiagramProfessions'
 import VerticalMenu from '@/components/VerticalMenu'
 import HeroArea from '@/components/HeroArea'
+import Synopsis from '@/components/Synopsis'
 
 export default {
-  components: { VerticalMenu, VennDiagramProfessions, HeroArea },
+  components: { VerticalMenu, HeroArea, Synopsis },
   data () {
     return {}
   }
