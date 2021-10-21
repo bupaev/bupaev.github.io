037230f4b3f3c02a1b31d08fd4ab190db2f6cfa0
Pavel Buramensky <bupaev@gmail.com>
2021-10-21 19:43:10 +0600

    Remove unused Nuxt modules

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,12 +58,7 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/content
-    '@nuxt/content'
-  ],
+  modules: [],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
