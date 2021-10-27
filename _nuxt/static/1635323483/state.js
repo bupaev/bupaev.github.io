e51e8286b249a5bfbf6f1cc6fdfe9851bc9f08d7
bupaev <bupaev@users.noreply.github.com>
2021-10-27 08:31:24 +0000

    deploy: 73514e3699c163326e0fc0621f5e60b2765c5172

---
+++ _nuxt/static/1635323483/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmy-personal-site\u002F_nuxt\u002Fstatic\u002F1635323483",layout:"servicePage",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fmy-personal-site\u002F",assetsPath:"\u002Fmy-personal-site\u002F_nuxt\u002F",cdnURL:a}}}}(null));
