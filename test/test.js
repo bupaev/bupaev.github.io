b7b9816f648b1e9042ea90db1aff17bfb58f58d9
Pavel Buramensky <bupaev@gmail.com>
2021-11-09 02:38:15 +0600

    test: downgrade vue-jest version for better compatability with @nuxt/text-utils

--- test/test.js
+++ test/test.js
@@ -1,6 +1,9 @@
 import { setupTest, createPage } from '@nuxt/test-utils'
+import { mount } from '@vue/test-utils'
+import 'core-js'
+import CV from '@/pages/cv.vue'
 
-describe('browser', () => {
+describe.skip('browser', () => {
   setupTest({
     testDir: __dirname,
     fixture: 'example',
@@ -11,5 +14,12 @@ describe('browser', () => {
     const page = await createPage('/')
     const body = await page.innerHTML('body')
     expect(body).toContain('Failed')
+  })
+})
+
+describe('CV', () => {
+  test('is a Vue instance', () => {
+    const wrapper = mount(CV)
+    expect(wrapper.vm).toBeTruthy()
   })
 })
