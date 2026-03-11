1a4d77f63a17317776196fef976b5a434e9677d2
bupaev <bupaev@gmail.com>
2026-03-11 02:33:24 +0100

    fix(cd): rename partytown dir to avoid ftp tilde issue
    - Set partytown lib path to /partytown/ instead of /~partytown/
    - Remove ~partytown ftp exclusion since tilde no longer present

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,6 +18,7 @@ export default defineConfig({
         partytown({
             config: {
                 forward: ["dataLayer.push"],
+                lib: "/partytown/",
             },
         }),
         mdx()
